d7ebe367d91391b60b910bbe930d4a6036f241b2
B-singularity <skil1489@naver.com>
2025-01-27 00:37:24 +0900

    doing chapter18

---
+++ chapter_18/ex18.py
@@ -0,0 +1,44 @@
+from scipy import stats
+from empiricaldist import Pmf
+import inspect
+import numpy as np
+import sys
+sys.path.append("/Users/seong-gyeongjun/Downloads/Baysian_Practice")
+import my_util.plot as plot
+from scipy.stats import multinomial
+
+
+
+def make_gamma_dist(alpha, beta):
+    dist = stats.gamma(alpha, scale=1/beta)
+    dist.alpha = alpha
+    dist.beta = beta
+    return dist
+
+def update_gamma(prior, data):
+    k, t = data
+    alpha = prior.alpha + k
+    beta = prior.beta + t
+    return make_gamma_dist(alpha, beta)
+
+def make_beta(alpha, beta):
+    dist = stats.beta(alpha, beta)
+    dist.alpha = alpha
+    dist.beta = beta
+    return dist
+def update_beta(prior, data):
+    k, t = data
+    alpha = prior.alpha + k
+    beta = prior.beta + t - k
+    return make_beta(alpha, beta)
+
+data = 3, 2, 1
+n = np.sum(data)
+ps = 0.4, 0.3, 0.3
+
+print(multinomial.pmf(data, n, ps))
+
+
+
+
+
